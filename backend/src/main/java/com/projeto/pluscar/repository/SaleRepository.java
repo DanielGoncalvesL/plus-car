2af5cb80de8ef5401b5c41ea8e8d65a82173535e
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-14 02:07:40 -0300

    Criação do Repositorio SaleRepository

--- backend/src/main/java/com/projeto/pluscar/repository/SaleRepository.java
+++ backend/src/main/java/com/projeto/pluscar/repository/SaleRepository.java
@@ -1,10 +1,10 @@
 package com.projeto.pluscar.repository;
 
-import com.projeto.pluscar.model.VehicleForSale;
+import com.projeto.pluscar.model.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehicleForSaleRepository extends JpaRepository <VehicleForSale, Long> {
+public interface SaleRepository extends JpaRepository<Sale, Long> {
 
 }
